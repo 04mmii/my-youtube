7b9c16f0e3f0bcd3f8f4237b24bee8eb89e106af
04mmii <04mmii@naver.com>
2025-02-03 17:50:11 +0900

    메인 콘텐츠 작업

---
+++ src/components/contents/Website.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Website = () => {
+  return <div>Webd</div>;
+};
+
+export default Website;
--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer id="footer" role="contentinfo">
-      footer
+      <a href="mailto:04mmii@naver.com" rel="noopener noreferrer">
+        04mmii@naver.com
+      </a>
     </footer>
   );
 };
