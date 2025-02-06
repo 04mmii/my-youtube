e524ec8a34264e91372278a771df73b9bcbe425e
04mmii <04mmii@naver.com>
2025-02-06 16:44:23 +0900

    검색 및 유튜브 API 설정하기

---
+++ src/components/videos/VideoSearch.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const VideoSearch = ({ videos }) => {
+  if (!videos || videos.length === 0) {
+    return <div>No videos available</div>;
+  }
+  return (
+    <>
+      {videos.map((video, index) => (
+        <div className="video" key={index}>
+          <div className="video__thumb play__icon">
+            <Link
+              to={`/video/${video.id.videoId}`}
+              style={{
+                backgroundImage: `url(${video.snippet.thumbnails.high.url})`,
+              }}
+            ></Link>
+          </div>
+          <div className="video__info">
+            <div className="title">
+              <Link to={`/video/${video.id.videoId}`}>
+                {video.snippet.title}
+              </Link>
+            </div>
+            <div className="info">
+              <span className="author">
+                <Link to={`/channel/${video.snippet.channelId}`}>
+                  {video.snippet.channelTitle}
+                </Link>
+              </span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default VideoSearch;
